83eafad9d1579866ee79c00d48138953f34c3adb
lacie-life <00sao00ios00@gmail.com>
2021-02-24 20:25:41 +0700

    remove stop words example (english)

--- NLP/example/main.py
+++ NLP/example/main.py
@@ -4,17 +4,14 @@ import spacy
 text = """When learning data science, you shouldn't get discouraged!
 Challenges and setbacks aren't failures, they're just part of the journey. You've got this!"""
 
-def spacy_tokenizing(text):
-    nlp = English()
+def spacy_tokenizing(nlp, text):
     my_doc = nlp(text)
     token_list = []
     for token in my_doc:
         token_list.append(token.text)
     print(token_list)
 
-def spacy_sentencizer(text):
-    nlp = English()
-#    sbd = nlp.create_pipe('sentencizer')
+def spacy_sentencizer(nlp, text):
     nlp.add_pipe('sentencizer')
     doc = nlp(text)
     sents_list = []
@@ -27,8 +24,18 @@ def eng_stop_words(num):
     print('Number of stop words: %d' % len(spacy_stopwords))
     print('Some stop words example: %s' % list(spacy_stopwords)[:num])
 
+def remove_stopwords(nlp, text):
+    filtered_sent = []
+    doc = nlp(text)
+
+    for word in doc:
+        if word.is_stop == False:
+            filtered_sent.append(word)
+    return filtered_sent
 
 if __name__ == "__main__":
-    spacy_tokenizing(text)
-    spacy_sentencizer(text)
+    nlp = English()
+    spacy_tokenizing(nlp, text)
+    spacy_sentencizer(nlp, text)
     eng_stop_words(20)
+    print(remove_stopwords(nlp, text))
