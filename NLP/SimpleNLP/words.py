2a55aa284aac4a085ed12be05d4665f3c96fb848
lam <tunglam3898@gmail.com>
2021-02-27 18:23:07 +0700

    update class Chunk
    [<MyKMS>][<Lam>][SimpleNLP] <Create SImpleNLP Class>
    
    [DESC] <Created SImpleNLP Class>
    [CHANGELOG] 
    * Creat class Chunk => done
    [BUILD] OK

--- NLP/SimpleNLP/words.py
+++ NLP/SimpleNLP/words.py
@@ -2,3 +2,11 @@ class Words:
     def __init__(self):
         self.text = ''
         self.tagging = ''
+
+
+class Chunk:
+    def __init__(self):
+        self.text = ''
+        self.root_text = ''
+        self.root_dep = ''
+        self.root_head_text = ''
