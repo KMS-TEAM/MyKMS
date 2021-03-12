21128c55a334cbdc35ec53b2c803d2e1b139b9a5
lam <tunglam3898@gmail.com>
2021-03-12 17:53:17 +0700

    code ngu

--- NLP/SimpleNLP/main.py
+++ NLP/SimpleNLP/main.py
@@ -1,37 +1,19 @@
-from simpleNLP import SimpleNLP
-import logging
 from neo4j import GraphDatabase
-from neo4j.exceptions import ServiceUnavailable
-if __name__ == "__main__":
-    node = []
-    test = SimpleNLP(u"data/input/test.txt")
-    data = test.read_txt()
-#    print(len(data))
-#    nlp = test.word(data[1])
-    data = test.get_entitiy_pairs()
-    for nlp in data:
-        node.append(nlp)
-    print (node)
-    connect = GraphDatabase.driver(uri="neo4j://localhost:7687", auth=("neo4j", "1"))
-    ses = connect.session()
-    person_name = "Johan"
-    something = "surfing"
-    person = "Emil"
-    local = "Sweden"
-    q1= (
-            "MATCH(js:Person) - [:KNOWS]-() - [: KNOWS]-(surfer)"
-            "WHERE js.name = $person_name AND surfer.hobby = $something "
-            "RETURN DISTINCT surfer"
-    )
-    q2 = (
-        "CREATE(ee:Person {name: $person, from: $local, klout: 99})"
-    )
 
-    ses.run(q1, person_name=person_name, something=something)
-    ses.run(q2, person=person, local=local)
+driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", "1"))
 
-#   for n in nlp:
-#      print(n.text + " " + n.tagging)
-
-
-
+def add_friend(tx, name, lable, location, phonenumber):
+    tx.run("CREATE (a:Person {name: $name})", name=name)
+    return None
+def print_friends(tx, name):
+    result = tx.run("MATCH (a:Person {name: $name})", name=name)
+    return result.single()
+def update_friends(tx, live, name):
+    tx.run ("MATCH (a:Person {name: $name})"
+            "SET a.live = $live", live=live, name = name)
+    return None
+with driver.session() as session:
+#    session.write_transaction(add_friend, "Arthur")
+    session.write_transaction(update_friends, "HongKong", "Arthur")
+    session.read_transaction(print_friends, "Arthur")
+driver.close()
