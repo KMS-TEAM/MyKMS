9f9376b22245e3160bdf7c8961ea4dcd639d5e58
lacie-life <00sao00ios00@gmail.com>
2021-03-19 17:39:57 +0700

    update

--- NLP/SimpleNLP/networkxapi.py
+++
@@ -1,3 +0,0 @@
-# Using simpleNLP => processing text data => entity pairs + relations
-# Using neo4japi => create node in neo4j from data got in simplNLP
-# Some function to get data from neo4j (delete, find, update, add)
--- NLP/SimpleNLP/py2neo.py
+++
@@ -1,6 +0,0 @@
-from py2neo.data import Node, Relationship
-driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", "1"))
-            a = Node("Person", name="Alice")
-            b = Node("Person", name="Bob")
-            ab = Relationship(a, "KNOWS", b)
-            ab
